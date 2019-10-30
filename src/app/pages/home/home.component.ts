8b6d45d2c1e2455b32fe725643d29f30a3ed722c
Brocel <geenens.prof@gmail.com>
2019-10-30 15:07:35 +0100

    Edit de home template : bouton test ouverture de la modale bookingCalendar (à compléter)

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { BookingcalendarComponent } from '../../modals/bookingcalendar/bookingcalendar.component';
 
 export interface Periode {
   value: string;
@@ -12,7 +14,7 @@ export interface Periode {
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { } // dialog->modale bookingCalendar
 
   ngOnInit() {
   }
@@ -20,4 +22,17 @@ export class HomeComponent implements OnInit {
     {value: 'matin-0', viewValue: 'Matin'},
     {value: 'apresmidi-1', viewValue: 'Après-midi'},
   ];
+
+  //Modale bookingCalendar
+  openBookingCalendar(): void {
+    const dialogRef = this.dialog.open(BookingcalendarComponent, {
+      width: '250px',
+      //data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      //this.animal = result;
+    });
+  }
 }
