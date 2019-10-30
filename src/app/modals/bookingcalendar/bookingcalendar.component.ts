2fd89dc04c9547d0c417b706e3d9f370dc23b892
Brocel <geenens.prof@gmail.com>
2019-10-30 19:59:04 +0100

    import de MatDialogRef / ajout de bookingCalendarDialogRef dans le constructeur

--- src/app/modals/bookingcalendar/bookingcalendar.component.ts
+++ src/app/modals/bookingcalendar/bookingcalendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from "@angular/material/dialog";
 
 @Component({
   selector: 'app-bookingcalendar',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingcalendarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public bookingCalendarDialogRef: MatDialogRef<BookingcalendarComponent>) { }
 
   ngOnInit() {
   }
