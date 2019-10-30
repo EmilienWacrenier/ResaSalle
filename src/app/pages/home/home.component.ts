1b855cb48a6359c243aa159fbd29594e708eff0a
Brocel <geenens.prof@gmail.com>
2019-10-30 21:25:22 +0100

    retrait de : import de bookingDetailsComponent / ajout de bookingDetailsDialog dans le constructeur

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { BookingcalendarComponent } from 'src/app/modals/bookingcalendar/bookingcalendar.component';
-import { BookingdetailsComponent } from 'src/app/modals/bookingdetails/bookingdetails.component';
 
 export interface Periode {
   value: string;
@@ -15,7 +14,7 @@ export interface Periode {
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private bookingCalendarDialog: MatDialog, private bookingDetailsDialog: MatDialog) { }
+  constructor(private bookingCalendarDialog: MatDialog) { }
 
   ngOnInit() {
   }
