9563c7d9114a720a047cf435580b35e700f997ad
Brocel <geenens.prof@gmail.com>
2019-10-30 21:36:21 +0100

    import de MatDialogRef et ajout de bookingDetailsDialogRef dans le constructeur

--- src/app/modals/bookingdetails/bookingdetails.component.ts
+++ src/app/modals/bookingdetails/bookingdetails.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from "@angular/material/dialog";
 
 @Component({
   selector: 'app-bookingdetails',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingdetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor( public bookingDetailsDialogRef: MatDialogRef<BookingdetailsComponent> ) { }
 
   ngOnInit() {
   }
