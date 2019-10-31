39eb10ad1e710a0c84ddc107d25a291058860283
Corentin <cochaux.c@hotmail.fr>
2019-10-31 13:55:34 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,13 +11,23 @@ import { UserlayoutComponent } from './layouts/userlayout/userlayout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SearchComponent } from './pages/search/search.component';
-import { ResaDateComponent } from './pages/search/steps/resa-date/resa-date.component';
-import { ResaRecurrenceComponent } from './pages/search/steps/resa-recurrence/resa-recurrence.component';
-import { ResaResultatComponent } from './pages/search/steps/resa-resultat/resa-resultat.component';
 import { BookingcalendarComponent } from './modals/bookingcalendar/bookingcalendar.component';
 import { BookingdetailsComponent } from './modals/bookingdetails/bookingdetails.component';
-import { ResaCaracteristiqueComponent } from './pages/search/steps/resa-caracteristique/resa-caracteristique.component';
- 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatInputModule} from '@angular/material';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
@@ -28,16 +39,29 @@ import { ResaCaracteristiqueComponent } from './pages/search/steps/resa-caracter
     HomeComponent,
     DashboardComponent,
     SearchComponent,
-    ResaDateComponent,
-    ResaRecurrenceComponent,
-    ResaResultatComponent,
     BookingcalendarComponent,
     BookingdetailsComponent,
-    ResaCaracteristiqueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatSidenavModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatStepperModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/sides/sidebar/sidebar.component.ts
+++ src/app/sides/sidebar/sidebar.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
 
+export class SidebarComponent implements OnInit {
+  home : boolean;
+  dashboard : boolean;
+  search : boolean;
   constructor() { }
 
   ngOnInit() {
+    this.home= false;
+    this.dashboard = true;
+    this.search = true;
+  }
+  Home(){
+    this.home = false;
+    this.dashboard = true;
+    this.search = true;
+  }
+  Dashboard(){
+    this.home = true;
+    this.dashboard = false;
+    this.search = true;
+  }
+  Search(){
+    this.home = true;
+    this.dashboard = true;
+    this.search = false;
   }
 
 }
