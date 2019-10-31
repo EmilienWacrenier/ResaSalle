7b49544025fd474ae8c87784eba1a2d525650bf0
LEPLAE <louan.leplae.external@atos.net>
2019-10-31 11:17:35 +0100

    bouton fonctionnels

--- src/app/sides/sidebar/sidebar.component.ts
+++ src/app/sides/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
 
 
 @Component({
@@ -10,6 +11,8 @@ export class SidebarComponent implements OnInit {
   home : boolean;
   dashboard : boolean;
   search : boolean;
+  el : string;
+
   constructor() { }
 
   ngOnInit() {
@@ -18,19 +21,42 @@ export class SidebarComponent implements OnInit {
     this.search = true;
   }
   Home(){
+    this.activate("button_home");
+    this.desactivate("button_dashboard");
+    this.desactivate("button_search");
     this.home = false;
     this.dashboard = true;
     this.search = true;
   }
   Dashboard(){
+    this.desactivate("button_home");
+    this.activate("button_dashboard");
+    this.desactivate("button_search");
     this.home = true;
     this.dashboard = false;
     this.search = true;
   }
   Search(){
+    this.desactivate("button_home");
+    this.desactivate("button_dashboard");
+    this.activate("button_search");
     this.home = true;
     this.dashboard = true;
     this.search = false;
+  }
+  activate(st : string){
+    var square =document.getElementById(st);
+    square.style.color= "white" ;
+    square.style.borderLeftStyle = "solid";
+    square.style.borderLeftColor = "rgba(255, 255, 255, 0.774)";
+
+
+  }
+  desactivate(st : string){
+    var square =document.getElementById(st);
+    square.style.color= "rgba(255, 255, 255, 0.555)" ;
+    square.style.borderLeftStyle = "none";
+    
   }
 
 }
